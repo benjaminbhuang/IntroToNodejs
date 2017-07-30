3c87278f1e11973f9ff05a5bf02e96fdd1f26943
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-07-30 15:10:28 -0400

    add users api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -12,6 +12,17 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', function (req, res) {
+
+    res.json(
+        [
+            {"id": 1, "firstName": "Alice", "lastName": "Smith", "email": "alice@aberlyn.com"},
+            {"id": 2, "firstName": "Bobby", "lastName": "Jones", "email": "bobby@aberlyn.com"},
+            {"id": 3, "firstName": "Charlie", "lastName": "Doe", "email": "charlie@aberlyn.com"}
+        ]
+    );
+});
+
 app.listen(port, function (err) {
     if(err){
         console.log(err);
